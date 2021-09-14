27d0e69b7d6e53d185dcb451ea57b7a12db16108
caroline <carolc.lima17@gmail.com>
2021-09-14 11:12:18 -0300

    finalizou

--- src/index.js
+++ src/index.js
@@ -18,13 +18,21 @@ app.get('/matricula', async (req, resp) => {
 app.post('/matricula', async (req, resp) => {
     try {
         let { nome, chamada, curso, turma } = req.body;
-        let r = await db.tb_matricula.create({
-            nm_aluno: nome,
-            nr_chamada: chamada,
-            nm_curso: curso,
-            nm_turma: turma
-        })
-        resp.send(r);
+        let consulta = await db.tb_matricula.findOne({ where: {nr_chamada: chamada, nm_turma: turma} })
+        if(consulta != null){
+            resp.send({erro: 'Aluno já existe!'})
+        } else
+            if(nome == "" || chamada == "" || curso == "" || turma == ""){
+                resp.send({ erro: "Todos os campos são obrigatórios!" })
+            } else {
+                let r = await db.tb_matricula.create({
+                    nm_aluno: nome,
+                    nr_chamada: chamada,
+                    nm_curso: curso,
+                    nm_turma: turma
+                })
+                resp.send(r);
+            }
     } catch (e) {
         resp.send({ erro: e.toString() })
     }
